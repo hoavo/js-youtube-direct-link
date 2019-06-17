fe92093b289f647a8f4421fe1b3d059bd73fe1eb
Hoa Vo <vovanhoait@gmail.com>
2019-06-17 13:23:43 +0700

    update res fail

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@ const port = process.env.PORT || 3000;
 app.get("/youtube", (req, res) => {
   const videoId = req.query.id;
   var url = `http://www.youtube.com/watch?v=${videoId}`;
-  youtubedl.getInfo(url, [], function(err, info) {
-    if (err) throw err;
-    const video = { url: info.url };
-    res.json(video);
-  });
+  try {
+    youtubedl.getInfo(url, [], function(err, info) {
+      if (err) throw err;
+      const video = { success: true, url: info.url };
+      res.json(video);
+    });
+  } catch (error) {
+    res.json({ success: false });
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
